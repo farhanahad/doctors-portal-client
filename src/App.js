eca316abe4832f1f5bc5df4a0349f9feb5a7ae9c
Farhan <farhanahad3@gmail.com>
2021-12-03 21:37:30 +0600

    added some parts

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import {
   Route
 } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
+import Appointment from './Pages/Appointment/Appointment/Appointment';
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/appointment">
+            <Appointment></Appointment>
           </Route>
         </Switch>
       </Router>
